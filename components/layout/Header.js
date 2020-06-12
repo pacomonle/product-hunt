3c0f08047ea481f4bd7601e79ab8378088b603e7
NolitoxD <franciscomonleonleal@gmail.com>
2020-06-12 07:54:26 +0200

    barra de navegacion

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,27 +2,89 @@ import React from "react";
 import Link from "next/link";
 import Buscar from "../ui/Buscar";
 import Navegacion from "../layout/Navegacion";
+import Boton from "../ui/Boton";
+// css de emotion/core
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const ContenedorHeather = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+const Logo = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-size: 700;
+  font-family: "Roboto Slab", serif;
+  margin-right: 2rem;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
 
 const Header = () => {
+
+const usuario = false
+
+
   return (
-    <div>
-      <div>
-        <div>
-          <p>P</p>
+    <header
+      css={css`
+        border-bottom: 2px solid var(--gris3);
+        padding: 1rem 0;
+      `}
+    >
+      <ContenedorHeather>
+        <div css={css`
+        display:flex;
+        align-items: center;
+        `}>
+          <Link href="/">
+            <Logo>P</Logo>
+          </Link>
+
           {/**BUscador aqui */}
           <Buscar></Buscar>
           {/**Nav aqui aqui */}
           <Navegacion></Navegacion>
         </div>
-        <div>
-          {/**Administracion usuarios */}
-          <p>Hola: Paco</p>
-          <button type="button">Cerrar sesion</button>
-          <Link href="/">Login</Link>
-          <Link href="/">Crear cuenta</Link>
-        </div>
+        <div
+        css={css`
+            display: flex;
+            align-items: center;
+        `}>
+        { usuario ? (
+          <>
+              <p
+                  css={css`
+                      margin-right: 2rem;
+                  `}
+              >Hola: Paco</p>
+              <Boton
+                  bgColor="true"
+                //  onClick={() => firebase.cerrarSesion() }
+              >Cerrar Sesión</Boton>
+          </>
+      ) : (
+          <>
+              <Link href="/login">
+                  <Boton bgColor="true">Login</Boton>
+              </Link>
+              <Link href="/crear-cuenta">
+                  <Boton>Crear Cuenta</Boton>
+              </Link>
+          </>
+      ) }
       </div>
-    </div>
+      </ContenedorHeather>
+    </header>
   );
 };
 
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,47 +1,81 @@
 import React, { Fragment } from "react";
 import Header from "../layout/Header";
+// importar el head (metadatos) del html
+import Head from "next/head";
 // importar emotion para hacer CSS
 import { Global, css } from "@emotion/core";
-
 
 const Layout = (props) => {
   return (
     <Fragment>
       <Global
-      styles={css`
-           :root{
-              --gris: #3d3d3d;
-              --gris2: #6F6F6F;
-              --naranja: #DA552F;
-           }
-           html{
-               font-size: 62.5%;
-               box-sizing: border-box;
-           }
-           *, *:before, *:after{
+        styles={css`
+          :root {
+            --gris: #3d3d3d;
+            --gris2: #6f6f6f;
+            --gris3: #e1e1e1;
+            --naranja: #da552f;
+          }
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+          *,
+          *:before,
+          *:after {
             box-sizing: inherit;
-           }
-           body{
-               font-size: 1.6rem; /** 16px */
-               line-height: 1.5;
-           }
-           h1, h2, h3 {
-               margin: 0 0 2rem 0;
-               line-height: 1.5;
-           }
-           ul{
-               list-style: none;
-               margin: 0px;
-               padding: 0px;
-           }
-           a{
-               text-decoration: none;
-           }
-      `}
-      
+          }
+          body {
+            font-size: 1.6rem; /** 16px */
+            line-height: 1.5;
+          }
+          h1,
+          h2,
+          h3 {
+            margin: 0 0 2rem 0;
+            line-height: 1.5;
+          }
+          h1,
+          h2 {
+           
+            font-family: "Roboto Slab", serif;
+            font-weight: 800;
+          }
+          h3{
+            font-family: "PT Sans", sans-serif;
+            font-weight: 700;
+          }
+          body{
+            font-family: "PT Sans", sans-serif;
+            font-weight: 700;
+          }
+          ul {
+            list-style: none;
+            margin: 0px;
+            padding: 0px;
+          }
+          a {
+            text-decoration: none;
+          }
+        `}
       ></Global>
+      <Head>
+        <html lang="es" />
+        <title>Product Hunt Firebas y Next js</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU="
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@500;800&display=swap"
+          rel="stylesheet"
+        />
+
+        <link rel="stylesheet" type="text/css" href="/static/css/app.css" />
+      </Head>
       <Header></Header>
-     
 
       <main>{props.children}</main>
     </Fragment>
